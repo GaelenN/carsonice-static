580c9335ecc67d6d621d801617f5078e38c5ff37
Gaelen Norman <gaelen@gaelennorman.ca>
2020-10-05 14:05:28 -0700

    rulebooks

--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -19,18 +19,18 @@ class Contact extends Component {
         **NOTE** Time Attack requires a CACC competition licence to participate. 
         </div>
         <ul class="list-group">
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">Auto Slalom Rule Book (CACC)</a></li>
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">IceX supplemental regulations ( Cars On Ice )</a></li>
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">Time Attack Rule Book (CACC)</a></li>
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">Time Attack Supplemental Regulations ( Cars On Ice )</a></li>
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">IceX and Time attack Tech Forms (Please print and fill out, making sure your car is compliant)</a></li>
+          <li class="list-group-item"><a href="http://www.caccautosport.org/wp-content/uploads/2017/01/2017-CACC-Autoslalom-Regulations.pdf" target="_blank" rel="noopener noreferrer">Auto Slalom Rule Book (CACC)</a></li>
+          <li class="list-group-item"><a href={require('../docs/2020-carsonice-supreg.pdf')} target="_blank" rel="noopener noreferrer">IceX supplemental regulations ( Cars On Ice )</a></li>
+          <li class="list-group-item"><a href="http://www.caccautosport.org/wp-content/uploads/2017/12/2019-Time-Attack-Rulebook-V1-1.pdf" target="_blank" rel="noopener noreferrer">Time Attack Rule Book (CACC)</a></li>
+          <li class="list-group-item"><a href={require('../docs/2020-carsonice-supreg.pdf')} target="_blank" rel="noopener noreferrer">Time Attack Supplemental Regulations ( Cars On Ice )</a></li>
+          <li class="list-group-item"><a href={require('../docs/2020-carsonice-techform.pdf')} target="_blank" rel="noopener noreferrer">IceX and Time attack Tech Forms (Please print and fill out, making sure your car is compliant)</a></li>
         </ul>
 
         <h3 className="my-3">Ice Racing (Wheel to Wheel) Car Preparation</h3>
         <ul class="list-group">
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">CACC GCR for Car building specifications</a></li>
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">CACC Ice Racing Discipline Car Preparation Specifications</a></li>
-          <li class="list-group-item"><a href="" target="_blank" rel="noopener noreferrer">Cars on Ice Supplemental Rule Book</a></li>
+          <li class="list-group-item"><a href="http://www.caccautosport.org/wp-content/uploads/2017/12/2019-CACC-GCR-V1-1.pdf" target="_blank" rel="noopener noreferrer">CACC GCR for Car building specifications</a></li>
+          <li class="list-group-item"><a href={require('../docs/2020-carsonice-rules.pdf')} target="_blank" rel="noopener noreferrer">CACC Ice Racing Discipline Car Preparation Specifications</a></li>
+          <li class="list-group-item"><a href={require('../docs/2020-carsonice-supreg.pdf')} target="_blank" rel="noopener noreferrer">Cars on Ice Supplemental Rule Book</a></li>
         </ul>
       </div>
     );
