0023d1cc073d115d48d84237a886e5b58920fbaa
Gaelen Norman <gaelen@gaelennorman.ca>
2020-10-06 10:31:54 -0700

    :add tech forms

--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import rules from '../docs/2020-carsonice-rules.pdf';
 import supregs from '../docs/2020-carsonice-supreg.pdf';
+import techforms from '../docs/2020-carsonice-techform.pdf';
  
 class Contact extends Component {
   render() {
@@ -16,13 +17,14 @@ class Contact extends Component {
           <li>KEEP THE LAKE CLEAN, this includes spills, food and drink waste, and cigarette butts. If it doesn’t belong in the lake, keep it off the ice.</li>
           <li>We want to keep a peaceful relationship with the locals. Be respectful, no hooning!</li>
         </ul>
-        <h3 className="my-3">IceX and Time Attack (No Passing) </h3>
+        <h3 className="my-3">IceX and Ice Attack (No Passing) </h3>
         <div class="alert alert-danger" role="alert">
-        **NOTE** Time Attack requires a CACC competition licence to participate. 
+        **NOTE** Ice Attack requires a CACC competition licence to participate. 
         </div>
         <ul class="list-group">
           <li class="list-group-item"><a href={supregs} target="_blank" rel="noopener noreferrer">Cars On Ice Rules</a></li>
           <li class="list-group-item"><a href={rules} target="_blank" rel="noopener noreferrer">Cars On Ice Supplemental Regulations</a></li>
+          <li class="list-group-item"><a href={techforms} target="_blank" rel="noopener noreferrer">Cars On Ice Tech Form</a></li>
         </ul>
 
       </div>
