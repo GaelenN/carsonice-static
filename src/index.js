dbff5a8dd623c5bce0389002ca27be8f931e5739
Gaelen Norman <gaelen@gaelennorman.ca>
2020-10-14 18:15:05 -0700

    Nav fix for mobile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import Main from "./Main";
 import './sass/custom.scss';
 import './sass/styles.scss';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-180628590-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
  
 ReactDOM.render(
   <Main/>, 
